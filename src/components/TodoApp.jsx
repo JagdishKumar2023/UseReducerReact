fd06ace304812d5c76a2e2812701937489258d8f
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-12-23 14:26:35 +0530

    New Changes:TODOS_ACTION

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,70 @@
+import React, { useReducer } from "react";
+
+const initialState = [];
+
+const TODOS_ACTION = {
+  ADD_TASK: "add_task",
+  DELETE_TASK: "delete_task",
+  REST_TODOS: "reset_todos",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case TODOS_ACTION.ADD_TASK:
+      return [
+        ...state,
+        {
+          id: state.length + 1,
+          name: action.payload,
+        },
+      ];
+
+    case TODOS_ACTION.DELETE_TASK:
+      return state.filter((d) => d.id !== action.payload);
+
+    case TODOS_ACTION.REST_TODOS:
+      init(action.payload);
+
+    default:
+      return state;
+  }
+}
+
+function init(initialState) {
+  const result = [...initialState, { id: "1", name: "reading" }];
+  return result;
+}
+
+export const TodoApp = () => {
+  const [todos, dispatch] = useReducer(reducer, initialState, init);
+
+  return (
+    <>
+      <h2>Todo List {todos.length}</h2>
+      Add New Task :
+      <input
+        type="text"
+        onBlur={(e) =>
+          dispatch({ type: TODOS_ACTION.ADD_TASK, payload: e.target.value })
+        }
+      />
+      {todos.map((todos) => (
+        <li key={todos.id}>
+          {todos.name}
+          <span>
+            <button
+              onClick={() =>
+                dispatch({
+                  type: TODOS_ACTION.DELETE_TASK,
+                  payload: todos.id,
+                })
+              }
+            >
+              Delete
+            </button>
+          </span>
+        </li>
+      ))}
+    </>
+  );
+};
